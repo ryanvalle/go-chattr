f993d32eb183b1aef679040872858ae25511ba94
Ryan Valle <ryanvalle@me.com>
2015-05-11 21:00:07 -0700

    Move html to a chat.html template

--- main.go
+++ main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>	
-				<body>
-					Let's Chat!
-				</body>
-			</html>
-		`))
+// Temp1 Represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	temp1    *template.Template
+}
+
+// ServeHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.temp1.Execute(w, nil)
+}
+
+func main() {
+	// Call the Root Directory
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// Start the Web Server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
